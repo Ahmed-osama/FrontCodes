30b0e3ae30f9f0e7a610a1afc4887a778e1f31e7
ahmedOsama <ahmod.osama@gmail.com>
2017-06-30 23:51:08 +0200

    comment

--- gulpfile.js
+++ gulpfile.js
@@ -42,14 +42,14 @@
 		return gulp
 			.src(
 				[
-					// 'src/pug/index-en.pug',
-					// 'src/pug/index-ar.pug',
+					'src/pug/index-en.pug',
+					'src/pug/index-ar.pug',
 					
 					// 'src/pug/icons-en.pug',
 					// 'src/pug/icons-ar.pug',
 
-					'src/pug/guide-en.pug',
-					'src/pug/guide-ar.pug',
+					// 'src/pug/guide-en.pug',
+					// 'src/pug/guide-ar.pug',
 
 				]
 			)
